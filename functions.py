423501b9ffe765789f231f600c35d3705dacce1f
abhijay24 <abhijay24.3.7@gmail.com>
2021-03-22 20:38:15 +0530

    project

---
+++ functions.py
@@ -0,0 +1,9 @@
+def countWordsFromFile():
+    fileName = input("Enter the file name")
+    numberOfWords = 0
+    file = open(fileName, 'r')
+    for line in file:
+        words = line.split()
+        numberOfWords = numberOfWords+len(words)
+    print(numberOfWords)
+countWordsFromFile()
